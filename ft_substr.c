c7d86650c269441128b3a445d65b2e4fa50c4bcf
BrandonTay_Legion <tayweiyong1007@gmail.com>
2024-03-07 16:50:24 +0800

    minor change on substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: twei-yo- <twei-yo-@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 12:13:46 by twei-yo-          #+#    #+#             */
-/*   Updated: 2024/03/07 12:29:21 by twei-yo-         ###   ########.fr       */
+/*   Updated: 2024/03/07 16:50:00 by twei-yo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	size_t	size_str;
 	char	*substr;
 
-	if (!str)
-		return (NULL);
-	size_str = ft_strlen(str);
-	if (start >= size_str)
-		return (ft_strdup(""));
-	if (len > size_str - start)
-		len = size_str - start;
-	substr = malloc(len + 1);
-	if (!substr)
-		return (NULL);
-	ft_strlcpy(substr, &str[start], len + 1);
-	return (substr);
+	
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	char *str
+	return 0;
 }
