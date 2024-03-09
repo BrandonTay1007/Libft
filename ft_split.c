68d33643e957a6a89483adeca764ad05b8c644fb
BrandonTay_Legion <tayweiyong1007@gmail.com>
2024-03-09 11:16:00 +0800

    add

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: twei-yo- <twei-yo-@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 18:06:33 by twei-yo-          #+#    #+#             */
-/*   Updated: 2024/03/09 10:18:42 by twei-yo-         ###   ########.fr       */
+/*   Updated: 2024/03/09 11:14:40 by twei-yo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,11 @@ char	**ft_split(char const *src, char c)
 	char	**arr;
 	size_t	i;
 	size_t	char_count;
-	int	arr_i;
+	char	**ptr;
 
 	i = 0;
-	arr_i = -1;
 	arr = malloc(countword(src, c) + 1);
-
+	ptr = arr;
 	while (src[i])
 	{
 		if (src[i] != c)
@@ -51,20 +50,21 @@ char	**ft_split(char const *src, char c)
 			char_count = 0;
 			while (src[i + char_count] != c)
 				char_count++;
-			arr[++arr_i] = malloc(char_count + 1);
-			ft_strlcpy(arr[arr_i], (char *)(src + i), char_count + 1);
-			i += char_count;
+			*arr = malloc(char_count + 1);
+			ft_strlcpy(*arr, (char *)(src + i), char_count + 1);
+			i += char_count + 1;
+			arr++;
 		}
 		else
 			i++;
 	}
-	arr[arr_i + 1] = NULL;
-	return (arr);
+	*(++arr) = NULL;
+	return (ptr);
 }
 
 int main(int argc, char const *argv[])
 {
-	char *str = "Hello";
+	char *str = "Hello JIdsd ad hsa";
 	char	sep = ' ';
 	char **arr = ft_split(str, sep);
 	while (*arr)
@@ -72,5 +72,4 @@ int main(int argc, char const *argv[])
 		puts(*arr);
 		arr++;
 	}
-	puts(arr[0]);
 }
