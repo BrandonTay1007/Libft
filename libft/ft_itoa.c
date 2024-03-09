e03e2543b3418e4330d8bfecd3a9087062567aa3
Tay Wei Yong <tayweiyong1007@gmail.com>
2024-03-09 17:09:02 +0800

    putnbr incomplete

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,45 +6,71 @@
 /*   By: twei-yo- <twei-yo-@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 12:54:13 by twei-yo-          #+#    #+#             */
-/*   Updated: 2024/03/09 13:12:40 by twei-yo-         ###   ########.fr       */
+/*   Updated: 2024/03/09 16:37:40 by twei-yo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	recur(int n)
-{
-	char	c;
-	if (n < 10)
-		c = n + '0';
+char	*recur(unsigned int nb, char *str, int is_neg)
+{	
+	if (nb < 10)
+	{
+		if (is_neg)
+		{
+			*str = '-';
+			str++;
+		}
+		*str = nb + '0';
+		str++;
+	}
 	else
 	{
-		recur(n / 10);
-		recur(n % 10);
+		str = recur(nb / 10, str, is_neg);
+		str = recur(nb % 10, str, 0);
 	}
-	return (c);
+	return (str);
 }
 
-char	*ft_itoa(int n)
+int	get_size(unsigned int n)
 {
-	size_t	size;
-	char 	*str;
+	int	size;
 
 	size = 0;
-	if (n < 0)
-		n = (unsigned int)(n * -1);
-	while (n / 10 != 0)
+	while (n != 0)
 	{
 		size++;
 		n = n / 10;
 	}
-	printf("%i", size);
-	str = malloc (size + 1)
-	return (NULL);
+	return (size);
+}
+char	*ft_itoa(int n)
+{
+	size_t			size;
+	char 			*str;
+	int	is_neg;
+	unsigned int	nb;
+
+	is_neg = 0;
+	size = get_size(n);
+	if (n < 0)
+	{
+		size++;
+		nb = (unsigned int) (n * -1);
+		is_neg = 1;
+	}
+	else
+		nb = n;
+	str = malloc((size + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	recur(nb, str, is_neg);
+	(str[size + 1]) = '\0';
+	return (str);
 }
 
-int main(int argc, char const *argv[])
+/* int main(int argc, char const *argv[])
 {
-	ft_itoa(1233);
-	return 0;
-}
+	char *a = ft_itoa(-1);
+	printf("%s",a);
+} */
